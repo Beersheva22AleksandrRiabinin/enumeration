74ae03d1269971ca1b211a7da07bab8162c1e6ad
AleksandrRiabinin <alexr2141@gmail.com>
2023-02-12 11:47:25 +0200

    hw 30 enum

---
+++ src/telran/measure/LengthUnit.java
@@ -0,0 +1,23 @@
+package telran.measure;
+
+public enum LengthUnit {
+
+	MM(1), CM(10), IN(25.4f), M(1000), KM(1_000_000);
+
+	float value;
+
+	LengthUnit(float value) {
+		this.value = value;
+	}
+
+	public Length between(Length l1, Length l2) {
+		float l1inMM = l1.getAmount() * l1.getUnit().getValue();
+		float l2inMM = l2.getAmount() * l2.getUnit().getValue();
+		return new Length((l2inMM - l1inMM) / value, LengthUnit.this);
+	}
+
+	public float getValue() {
+		return value;
+	}
+
+}
